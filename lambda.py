b6022986c9c34ac9b5b136bbfcba9d10610a1461
Emma <97735091+mquat@users.noreply.github.com>
2022-06-15 14:33:38 +0900

    practice_0615

--- lambda.py
+++ lambda.py
@@ -2,6 +2,9 @@
 #A lambda function can take any number of arguments, but can only have one expression.
 
 #lambda - basic examples
+from tabnanny import check
+
+
 x = lambda a : a + 10
 print(x(5))
 
@@ -26,3 +29,34 @@ result = list(map(plus_ten, [1,2,3]))
 
 #두 번째 방법 (lambda 함수 사용)
 result = list(map(lambda x: x + 10, [1,2,3]))
+
+#-----------------------------------------------------------------
+#아래와 같이 내부 콜백함수(어떤 이벤트가 발생했을 때 호출되는 함수)를 작성할 때 lambda 사용 
+#그렇지 않으면 def func1, def func2, def func3 등처럼 함수를 따로 정의해야 한다 
+Lambdas = [
+    lambda x : x ** 2,
+    lambda x : x ** 3,
+    lambda x : x ** 4
+]
+
+for lambda_callback in Lambdas:
+    print(lambda_callback(2))
+
+
+#추가 연습문제 - lambda + if 조건문 
+#lambda함수에서 if조건문은 1)무조건 else문이 있어야 하며, 2) 콜론을 붙이지 않는다 그리고 3)elif를 쓸 수 없다 
+lambdas = [
+    lambda x : 'SHORT_PASSWORD' if len(x) < 8 else None,
+    lambda x : 'NO_CAPITAL_LETTER_PASSWORD' if not any(char.isupper() for char in x) else None
+]
+
+def check_password_using_lambda(password):
+    for f in lambdas:
+        if f(password) is not None:
+            return f(password)
+
+    return True
+
+print(check_password_using_lambda('123'))
+print(check_password_using_lambda('123456789f'))
+print(check_password_using_lambda('123456789F'))
