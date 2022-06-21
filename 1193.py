e70ae874112e55f1f5728f739a2cbbdacd6cb6f2
Emma <97735091+mquat@users.noreply.github.com>
2022-06-21 17:37:16 +0900

    practice_0621

--- 1193.py
+++ 1193.py
@@ -1,3 +1,15 @@
-#분수찾기
+N = int(input())
 
+line = 1
+while N>line:
+    N -= line
+    line += 1
 
+if line%2 == 0:
+    A = N
+    B = line-N+1
+else:
+    A = line-N+1
+    B = N
+
+print(A, '/', B, sep="")
