a2ea59a983f20d32f6d222686d4089d819626468
Emma <97735091+mquat@users.noreply.github.com>
2022-06-24 16:37:19 +0900

    backjoon_upto0624

--- 11653.py
+++ 11653.py
@@ -8,7 +8,6 @@ def factorize(num):
             num = num/i
         elif num%i != 0:
             i += 1
-    
-    return 
 
+#print(factorize(N)) -> facotrize 함수에 return값이 없기 때문에, print를 하면 None이 같이 출력된다. 
 factorize(N)
