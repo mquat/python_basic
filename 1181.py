c9075ef03f8e09d083f17b02fc8c1d137fb21dbe
Emma <97735091+mquat@users.noreply.github.com>
2022-07-01 23:57:24 +0900

    backjoon_upto0701

--- 1181.py
+++ 1181.py
@@ -1,8 +1,13 @@
-N = int(input())
+#기존 방법 (204ms) - input()은 줄바꿈('\n\')을 삭제하는 작업을 거친다 
+# N = int(input())
+# words = list(set([input() for _ in range(N)]))
 
-words = list(set([input() for _ in range(N)]))
+#더 빠른 방법 (176ms) - sys.stdin.readline()은 줄바꿈을 삭제시키지 않는다 
+import sys
 
-# words = list(set(words))
+N = int(sys.stdin.readline())
+words = list(set([sys.stdin.readline().strip() for i in range(N)]))     #readline은 '\n\'을 포팜하므로, strip()처리를 해야한다 
+
 words.sort()
 words.sort(key=len)
 
