01148ed02a5e3efea2d4f2f7764a9e4de62c8bf8
Emma <97735091+mquat@users.noreply.github.com>
2022-07-02 16:34:17 +0900

    backjoon_upto0702

---
+++ 11729.py
@@ -0,0 +1,19 @@
+#하노이의 탑 
+
+K = int(input())
+
+def hanoi_move(num,a,b,c):
+    if num == 1:
+        print(a,c)
+    else:
+        hanoi_move(num-1,a,c,b)
+        print(a,c)
+        hanoi_move(num-1,b,a,c)
+
+cnt = 0
+
+for _ in range(K):
+    cnt = cnt *2 + 1
+
+print(cnt)
+hanoi_move(K,1,2,3)
