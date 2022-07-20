f395ed1696fff82632f76579016df76443df8acd
Emma <97735091+mquat@users.noreply.github.com>
2022-07-20 22:53:33 +0900

    backjoon_upto0720

--- 11651.py
+++ 11651.py
@@ -1,5 +1,17 @@
 N = int(input())
 location = []
+
+#기본 풀이
+for _ in range(N):
+    X,Y = input().split()
+    location.append([int(Y),int(X)])
+
+location.sort() #오름차순이 default, 추가 설정 필요 없음 
+
+for n in location:
+    print(n[1],n[0])
+
+#추가 풀이
 for _ in range(N):
     X,Y = input().split()
     location.append((int(X),int(Y)))
