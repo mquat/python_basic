3ba6cebe83e029754928dc1702d951eeb9a4aa56
Emma <97735091+mquat@users.noreply.github.com>
2022-08-05 22:38:09 +0900

    backjoon_upto0805

---
+++ 10845.py
@@ -0,0 +1,39 @@
+N = int(input())
+
+order_list = []
+for _ in range(N):
+    order = tuple(map(str, input().split()))
+    order_list.append(order)
+
+result = [] 
+for order in order_list:
+    if order[0] == 'push':
+        result.append(int(order[1]))
+
+    elif order[0] == 'pop':
+        if result: 
+            print(int(result[0]))
+            del result[0]
+        else:
+            print(-1)
+
+    elif order[0] == 'size': 
+        print(len(result))
+
+    elif order[0] == 'empty':
+        if result:
+            print(0)
+        else:
+            print(1)
+
+    elif order[0] == 'front':
+        if result: 
+            print(result[0])
+        else:
+            print(-1)
+
+    elif order[0] == 'back':
+        if result:
+            print(result[-1])
+        else:
+            print(-1)
