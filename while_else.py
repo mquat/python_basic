f27572059734d524bbe117d9f39b31214eccb919
Emma <97735091+mquat@users.noreply.github.com>
2022-08-28 23:57:33 +0900

    practice_upto0828

---
+++ while_else.py
@@ -0,0 +1,7 @@
+#with the else statement, a block of code can be run when the condition no longer is true
+i = 1
+while i < 6:
+    print(i)
+    i += 1
+else:
+    print('i is no longer less than 6')
