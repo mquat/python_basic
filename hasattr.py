4368b4ca743041c837dcb00a5ab1c790b38997da
Emma <97735091+mquat@users.noreply.github.com>
2022-12-11 23:21:05 +0900

    practice_upto1211

--- hasattr.py
+++ hasattr.py
@@ -1,6 +1,6 @@
 class myAttribute:
     a = 1
-
+    
     def b(self):
         pass
 
@@ -13,3 +13,7 @@ print(getattr(myAttribute, 'a'))
 # a라는 변수에 값 9 설정하기(a=9)
 setattr(myAttribute, 'a', 9)
 print(myAttribute.a)
+
+# a라는 변수를 삭제하기
+delattr(myAttribute,'a')
+print(hasattr(myAttribute,'a'))     #false
