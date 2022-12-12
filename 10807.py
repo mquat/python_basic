9f2b17eaf217e2cee8c26ddb10f0ef29521bf9a6
Emma <97735091+mquat@users.noreply.github.com>
2022-12-12 21:59:16 +0900

    backjoon_upto1212

---
+++ 10807.py
@@ -0,0 +1,14 @@
+N = int(input())
+nums = input().split()
+v = input()
+
+#for문
+# cnt = 0
+# for num in nums:
+#     if num == v:
+#         cnt += 1
+
+# print(cnt)
+
+#count 사용
+print(nums.count(v))
