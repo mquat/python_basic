e76d6f5af4426937fb901d454235c25bdff16d60
Emma <97735091+mquat@users.noreply.github.com>
2023-03-21 23:00:35 +0900

    Add files via upload

--- decorator.py
+++ decorator.py
@@ -2,9 +2,9 @@ import time
 
 
 def elapsed(original_func):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         start = time.time()
-        result = original_func()    # 기존 함수를 수행
+        result = original_func(*args, **kwargs)    # 기존 함수를 수행, 전달받은 args/kwargs로 함수 수행
         end = time.time()
         print("함수 수행시간: %f 초" % (end - start))
         return result  # 기존 함수의 수행 결과를 리턴
@@ -15,5 +15,9 @@ def elapsed(original_func):
 def myfunc():
     print("함수가 실행됩니다.")
 
+@elapsed
+def mymsg(msg):
+    print("%s를 출력합니다" %msg)
 
 myfunc()
+mymsg("I need python") 
