2435a875ba25a8dce79fc8419b2ff11936916079
Emma <97735091+mquat@users.noreply.github.com>
2023-03-28 22:01:05 +0900

    Add files via upload

---
+++ 클래스속성.py
@@ -0,0 +1,17 @@
+#속성: 클래스 속성과 인스턴스 속성
+
+#클래스 속성 -> 클래스 자체에 속하는 속성, 따라서 모든 인스턴스에 공유됨
+class Person:
+    hobbies = []
+
+    def add_hobby(self, hobby):
+        self.hobbies.append(hobby)
+
+emma = Person()
+james = Person()
+
+emma.add_hobby('singing')
+emma.add_hobby('drawing')
+
+print(emma.hobbies)
+print(james.hobbies)    #james도 singing/drawing이 동일하게 출력된다
